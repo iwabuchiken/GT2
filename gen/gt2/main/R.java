ce3255e5ca2d7abd2222549fd3d76090fa1201ca
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-30 13:41:49 +0900

    master
    v-1.1
    
    D-------------
    1. Upto => Spinners: Set

--- gen/gt2/main/R.java
+++ gen/gt2/main/R.java
@@ -27,22 +27,27 @@ public final class R {
     }
     public static final class drawable {
         public static final int gt_project_50x50=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int gt_spinner_checked=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int spinner_check=0x7f020003;
     }
     public static final class id {
-        public static final int main_bt_patterns=0x7f060003;
-        public static final int main_bt_start=0x7f060008;
-        public static final int main_bt_stop=0x7f060009;
-        public static final int main_et_message=0x7f060002;
-        public static final int main_sp_minutes=0x7f060005;
-        public static final int main_sp_seconds=0x7f060007;
-        public static final int main_tv_message=0x7f060001;
-        public static final int main_tv_minutes=0x7f060004;
-        public static final int main_tv_seconds=0x7f060006;
-        public static final int main_tv_time=0x7f060000;
+        public static final int main_bt_patterns=0x7f070003;
+        public static final int main_bt_start=0x7f070008;
+        public static final int main_bt_stop=0x7f070009;
+        public static final int main_et_message=0x7f070002;
+        public static final int main_sp_minutes=0x7f070005;
+        public static final int main_sp_seconds=0x7f070007;
+        public static final int main_tv_message=0x7f070001;
+        public static final int main_tv_minutes=0x7f070004;
+        public static final int main_tv_seconds=0x7f070006;
+        public static final int main_tv_time=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int spinner_dropdown=0x7f030001;
+        public static final int spinner_dropdown_row=0x7f030002;
+        public static final int spinner_row=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
@@ -70,5 +75,8 @@ public final class R {
         /**  main.xml 
          */
         public static final int main_tv_time=0x7f05000c;
+    }
+    public static final class style {
+        public static final int SpinnerCheckedIcon=0x7f060000;
     }
 }
--- src/gt2/main/GT2Activity.java
+++ src/gt2/main/GT2Activity.java
@@ -1,15 +1,87 @@
 package gt2.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class GT2Activity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	/*----------------------------
+		 * 1. super
+		 * 2. Set content
+		 * 3. Spinner
+		 * 4. Listeners
+			----------------------------*/
+		
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        /*----------------------------
+		 * 3. Spinner
+			----------------------------*/
+        setup_spinner();
+		
     }//public void onCreate(Bundle savedInstanceState)
+
+	private void setup_spinner() {
+		/*----------------------------
+		 * 1. Get spinners
+		 * 
+			----------------------------*/
+		/*----------------------------
+		 * 1. Get spinners
+			----------------------------*/
+		Spinner sp_min = (Spinner) findViewById(R.id.main_sp_minutes);
+		Spinner sp_sec = (Spinner) findViewById(R.id.main_sp_seconds);
+		
+		/*----------------------------
+		 * 2. Get lists
+			----------------------------*/
+		List<Integer> minutes = new ArrayList<Integer>();
+		List<Integer> seconds = new ArrayList<Integer>();
+		
+		minutes.add(1); minutes.add(3); minutes.add(5);
+		minutes.add(10); minutes.add(15); minutes.add(20);
+		minutes.add(30); minutes.add(45); minutes.add(60);
+		
+		seconds.add(10); seconds.add(20); seconds.add(30);
+		seconds.add(40); seconds.add(50);
+		
+		/*----------------------------
+		 * 3. Setup adapters
+			----------------------------*/
+		// Min
+		ArrayAdapter<Integer> adapterMin = new ArrayAdapter<Integer>(
+//	              this, android.R.layout.simple_spinner_item, minutes);
+				this, R.layout.spinner_row, minutes);
+		
+		// Sec
+		ArrayAdapter<Integer> adapterSec = new ArrayAdapter<Integer>(
+//	              this, android.R.layout.simple_spinner_item, seconds);
+				this, R.layout.spinner_row, seconds);
+		
+		// Drop down view
+		adapterMin.setDropDownViewResource(
+//				android.R.layout.simple_spinner_dropdown_item);
+				R.layout.spinner_dropdown_row);
+		
+		adapterSec.setDropDownViewResource(
+//				android.R.layout.simple_spinner_dropdown_item);
+				R.layout.spinner_dropdown_row);
+
+		
+        /*----------------------------
+		 * 4. Set adapter
+			----------------------------*/
+		sp_min.setAdapter(adapterMin);
+		sp_sec.setAdapter(adapterSec);
+		
+	}//private void setup_spinner()
     
 }
